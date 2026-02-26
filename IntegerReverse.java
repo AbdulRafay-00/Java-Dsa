f62b104326a0f8a3c196942fb01cb972dba2834b
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-02-26 04:36:15 -0600

    integer reverse problem associated with leet code problen 5

---
+++ IntegerReverse.java
@@ -0,0 +1,15 @@
+public class IntegerReverse {
+    static void main(String[] args) {
+        int a = 21;
+        String s = Integer.toString(a);
+        String rev = "";
+
+        for(int i = s.length() -1; i >= 0; i--){
+            rev += s.charAt(i);
+        }
+
+        int finalRev = Integer.parseInt(rev);
+        System.out.println(finalRev);
+        // System.out.println(a.length());
+    }
+}
