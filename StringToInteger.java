8df054b15c6c48be6cb23a17c22c9d364283bf24
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-02-28 06:57:57 -0600

    done

--- StringToInteger.java
+++ StringToInteger.java
@@ -1,3 +1,47 @@
 public class StringToInteger {
     
+    public static void main(String[] args) {
+        String s = "-12a34";
+        String numStr = s.trim();
+        int num = 0;
+        int i = 0;
+        char a = ' ';
+
+        int sign = 1;
+        if(numStr.isEmpty()){
+            System.out.println(0);
+            
+        }
+        if (numStr.charAt(i) == '+') {
+            i++;
+        } else if (numStr.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        }
+        
+
+
+        for( ; i < numStr.length(); i++){
+            a = numStr.charAt(i);
+
+            if(a < '0' || a > '9') break;
+
+            if (a >= '0' && a <= '9') {
+
+                    if (num > Integer.MAX_VALUE / 10 ||
+                        ( num == Integer.MAX_VALUE / 10 
+                        && a - '0' >= (sign == 1 ? 7 : 8))) {
+
+                        System.out.println(sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE);
+                        
+                    }
+                num = num * 10 + a - '0';
+                
+                
+            }
+            
+        }
+        num = num * sign;
+        System.out.println(num);
+    }
 }
