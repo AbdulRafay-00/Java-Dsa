4bb341d14f78340be1ccd023e2a022a957192057
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-05-02 07:04:06 -0500

    full code pushed of haykeyproblem

--- String/hayKeyProblem.java
+++ String/hayKeyProblem.java
@@ -1,5 +1,37 @@
 package String;
 
 public class hayKeyProblem {
-    
+    public static void main(String[] args) {
+        String str = "mississippi";
+        String key = "issip";
+
+        int i = 0; // pointer for str
+        int j = 0; // pointer for key
+        int matchStart = -1; // tracks where the current match attempt started
+
+        while (i < str.length() && j < key.length()) {
+            if (str.charAt(i) == key.charAt(j)) {
+                if (j == 0) {
+                    matchStart = i; // record start of a potential match
+                }
+                j++;
+                i++;
+            } else {
+                // Mismatch: backtrack i to one past where match started, reset j
+                if (matchStart != -1) {
+                    i = matchStart + 1;
+                } else {
+                    i++;
+                }
+                j = 0;
+                matchStart = -1;
+            }
+        }
+
+        if (j == key.length()) {
+            System.out.println("Key found at index: " + matchStart);
+        } else {
+            System.out.println("Key not found.");
+        }
+    }
 }
