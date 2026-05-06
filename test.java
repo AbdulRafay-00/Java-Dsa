e5bd375f824af2fdc49430cdde7f883bbdeeff26
Abdul Rafay <iamrafay.00@gmail.com>
2026-05-06 21:38:33 +0500

    Update test.java

--- test.java
+++ test.java
@@ -12,7 +12,7 @@
 import java.util.LinkedList;
 
 class Test {
-    // link list test case
+    // link list test
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<>();
 
