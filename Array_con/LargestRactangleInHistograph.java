12166e90a05e69cc5b6249fb92ddc750179dc6b6
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-08-01 14:06:43 +0500

    LargestRactangleInHistograph problem solved time complexity O(n)

--- Array_con/LargestRactangleInHistograph.java
+++ Array_con/LargestRactangleInHistograph.java
@@ -1,8 +1,53 @@
 package Array_con;
 
+import java.util.Stack;
+
 public class LargestRactangleInHistograph {
-    //
-    public  static void main(String[] args) {
-        
+
+    public static int largestRectangleArea(int[] heights) {
+
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+
+        // Traverse all bars, plus one extra iteration with height = 0
+        for (int i = 0; i <= heights.length; i++) {
+
+            int currentHeight;
+
+            if (i == heights.length) {
+                currentHeight = 0;
+            } else {
+                currentHeight = heights[i];
+            }
+
+            while (!stack.isEmpty() && currentHeight < heights[stack.peek()]) {
+
+                int height = heights[stack.pop()];
+
+                int width;
+
+                if (stack.isEmpty()) {
+                    width = i;
+                } else {
+                    width = i - stack.peek() - 1;
+                }
+
+                int area = height * width;
+                maxArea = Math.max(maxArea, area);
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+
+        int[] heights = {2, 1, 5, 6, 2, 3};
+
+        int ans = largestRectangleArea(heights);
+
+        System.out.println("Largest Rectangle Area = " + ans);
     }
 }
