c06b5f17dcb26a2c64904620eb0359a9673f9cde
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-08-19 15:27:08 +0500

    pascalstriangle problem solved

--- TwoD_ArrayProblems/pascalTriangal.java
+++ TwoD_ArrayProblems/pascalTriangal.java
@@ -1,5 +1,32 @@
 package TwoD_ArrayProblems;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class pascalTriangal {
-    
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>(Collections.nCopies(i + 1, 1));
+
+            for (int j = 1; j < row.size() - 1; j++) {
+                row.set(j, triangle.get(i - 1).get(j) + triangle.get(i - 1).get(j - 1));
+            }
+
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+
+    public static void main(String[] args) {
+        int numRows = 5;
+
+        List<List<Integer>> result = generate(numRows);
+
+        System.out.println(result);
+    }
 }
